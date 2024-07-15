466948eb1bb3526a4f5f5cf67d0e11bbf3c2590c
Furqan Abid <thisismefurqan@gmail.com>
2024-07-16 02:14:48 +0500

    feat: added decode function

--- main.go
+++ main.go
@@ -5,10 +5,19 @@ import (
 )
 
 func main() {
-	indexKey := 69
+	indexKey := 7465
 
 	shortPath := Encode(indexKey)
 	shawtyUrl := "https://shaw.ty/" + shortPath
 
 	fmt.Println("Converted Short Path is: ", shawtyUrl)
+
+	decodedKey := Decode(shortPath)
+	fmt.Println("Decoded back is: ", decodedKey)
+
+	if decodedKey == indexKey {
+		fmt.Println("It works.")
+	} else {
+		fmt.Println("Nuh uh.")
+	}
 }
--- short.go
+++ short.go
@@ -1,23 +1,46 @@
 package main
 
 import (
+	"fmt"
+	"math"
+	"net/url"
 	"strings"
 )
 
 const base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+const base = 64
 
 func Encode(key int) string {
 	var shortStr strings.Builder
 
 	for key > 0 {
-		remainder := key % 64
+		remainder := key % int(base)
 		shortStr.WriteByte(base64Chars[remainder])
-		key = key / 64
+		key = key / int(base)
 	}
 
 	shortCode := reverse(shortStr.String())
 
 	return shortCode
+}
+
+func Decode(shortUrl string) int {
+	u, err := url.Parse(shortUrl)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	shortCode := u.Path
+
+	var result float64 = 0
+
+	for j := 0; j < len(shortCode); j++ {
+		char := shortCode[len(shortCode)-1-j]
+		value := float64(base64Value(char))
+		power := math.Pow(float64(base), float64(j))
+		result += value * power
+	}
+	return int(result)
 }
 
 func reverse(code string) (reverseCode string) {
@@ -27,4 +50,8 @@ func reverse(code string) (reverseCode string) {
 	}
 
 	return reverseCode
+}
+
+func base64Value(char byte) int {
+	return strings.IndexByte(string(base64Chars), char)
 }
